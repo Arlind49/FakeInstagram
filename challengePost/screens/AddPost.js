26196f311c43bfc15c8fb85a96291d243baf1bde
unknown <Student@digitalschool.local>
2025-01-22 17:16:00 +0100

    Update AddPost.js

--- challengePost/screens/AddPost.js
+++ challengePost/screens/AddPost.js
@@ -2,8 +2,24 @@ import React, {useState} from "react";
 import { View, Text ,TextInput, Button, StyleSheet } from "react-native-web";
 
 const AddPost =()=>{
+  const [title, setTitle]=useState('')
+  const [body, setBody]=useState('')
+  const [response, setResponse]=useState('')
+
   return(
-    <Text>Add Post Page</Text>
+<View> 
+   <Text>Create a Post</Text>
+   <TextInput  
+    value={title}
+    onChangeText={text=>setTitle(text)}
+    placeholder="Enter Title"
+   />
+    <TextInput  
+    value={body}
+    onChangeText={text=>setBody(text)}
+    placeholder="Enter Body"
+   />
+</View>
   );
 
 }
