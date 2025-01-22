793801bdae0fe61917aa01951c432578d4b55c36
Arlind Selimi <68119170+Arlind49@users.noreply.github.com>
2025-01-22 17:08:01 +0100

    adadad

---
+++ challengePost/screens/AddPost.js
@@ -0,0 +1,11 @@
+import React, {useState} from "react";
+import { View, Text ,TextInput, Button, StyleSheet } from "react-native-web";
+
+const AddPost =()=>{
+  return(
+    <Text>Add Post Page</Text>
+  );
+
+}
+
+export default AddPost;
